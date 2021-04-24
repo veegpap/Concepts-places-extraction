32a53851081fad7de316ad79b6c7398ee2616e91
Vangelis Papadias <veegpap@gmail.com>
2021-04-24 12:13:50 +0300

    Update ontology_visualization.R

--- Ontology/ontology_visualization.R
+++ Ontology/ontology_visualization.R
@@ -1,1 +1,9 @@
+library(visNetwork)
 
+load("ontology_example.RData")
+
+visNetwork(nodes_example, links_example) %>%
+  visOptions(highlightNearest = TRUE ) %>% 
+  visEdges(shadow = TRUE, 
+           arrows = list(to = list(enabled = TRUE, scaleFactor = 0.5)),
+           color = list(highlight = "red")) 
